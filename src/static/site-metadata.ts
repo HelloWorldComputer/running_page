0cfcfde3169648066b21ffca06060ea219a65d40
HelloWorldComputer <0011@111.com>
2025-03-04 15:04:29 +0800

    fourth commit

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -12,7 +12,7 @@ interface ISiteMetadataResult {
 const data: ISiteMetadataResult = {
   siteTitle: 'Running Page',
   siteUrl: 'http://xn--5gq955aoia.xn--ses554g/',
-  logo: 'https://www.picgo.net/image/151741005814-.pic.WuYLQm',
+  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
   navLinks: [
     {
